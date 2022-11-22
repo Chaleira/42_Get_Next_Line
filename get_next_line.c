293ba512b767c3aa91411e77d2bc176c56a90274
Chaleira <pedromchaleira@gmail.com>
2022-11-22 13:16:56 +0000

    prototype done

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: plopes-c <plopes-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 08:02:40 by plopes-c          #+#    #+#             */
-/*   Updated: 2022/11/22 08:03:17 by plopes-c         ###   ########.fr       */
+/*   Updated: 2022/11/22 08:11:50 by plopes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	
 }
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plopes-c <plopes-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 08:04:56 by plopes-c          #+#    #+#             */
+/*   Updated: 2022/11/22 08:08:26 by plopes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
